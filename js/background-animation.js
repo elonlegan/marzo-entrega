3ef97d3fc320c533e367083e0d0407066240b772
elonlegan <elonlegan@gmail.com>
2021-04-26 13:25:44 -0500

    finished background animation maybe change the background images

---
+++ js/background-animation.js
@@ -0,0 +1,19 @@
+const slide = document.querySelector('.slides');
+var slidePosition = 1;
+let slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].clientWidth;
+let counter = 0;
+
+function slideBackground() {
+	if (slidePosition % slideWidth === 0) {
+		slides = document.querySelectorAll('.slide');
+		var newSlide = slides[counter].cloneNode(true);
+		slides[0].remove();
+		slide.appendChild(newSlide);
+		slidePosition = slidePosition - slideWidth;
+	}
+	slide.style.transform = `translateX(${-slidePosition}px)`;
+	slidePosition++;
+}
+
+setInterval(slideBackground, 1);
