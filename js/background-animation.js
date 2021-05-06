755be9e5877888e47127057515f487a97c265ed3
elonlegan <elonlegan@gmail.com>
2021-05-06 17:49:50 -0500

    changed background code to optimize

--- js/background-animation.js
+++ js/background-animation.js
@@ -1,19 +1,64 @@
-const slide = document.querySelector('.slides');
+const slideContainer = document.querySelector('.slides');
 var slidePosition = 1;
-let slides = document.querySelectorAll('.slide');
-const slideWidth = slides[0].clientWidth;
-let counter = 0;
+let slidesURL = [
+	'img/Animados/bogota.svg',
+
+	'img/Animados/chicago.svg',
+
+	'img/Animados/hong_kong.svg',
+
+	'img/Animados/Japon.svg',
+
+	'img/Animados/lima.svg',
+
+	'img/Animados/londres.svg',
+
+	'img/Animados/new_york.svg',
+
+	'img/Animados/rio_janeiro.svg',
+
+	'img/Animados/roma.svg',
+
+	'img/Animados/toronto_.svg',
+];
+var slides;
+let backgroundCounter = 3;
+
+// insert first thre slides
+for (i = 0; i <= 2; i++) {
+	slideTemplate(slidesURL[i]);
+}
+const slideWidth = document.querySelector('.slide').clientWidth;
 
 function slideBackground() {
 	if (slidePosition % slideWidth === 0) {
-		slides = document.querySelectorAll('.slide');
-		var newSlide = slides[counter].cloneNode(true);
-		slides[0].remove();
-		slide.appendChild(newSlide);
 		slidePosition = slidePosition - slideWidth;
+		insertDelete();
 	}
-	slide.style.transform = `translateX(${-slidePosition}px)`;
-	slidePosition += 0.5;
+	slideContainer.style.transform = `translateX(${-slidePosition}px)`;
+	slidePosition += 1;
 }
 
-setInterval(slideBackground, 100);
+function insertDelete() {
+	slides = document.querySelectorAll('.slide');
+	slides[0].remove();
+	slideTemplate(slidesURL[backgroundCounter]);
+	if (backgroundCounter == slidesURL.length - 1) {
+		backgroundCounter = 0;
+	} else {
+		backgroundCounter++;
+	}
+}
+
+function slideTemplate(slideURL) {
+	var tag = document.createElement('div');
+	tag.innerHTML = `
+			<object type="image/svg+xml" data=${slideURL}></object>
+			`;
+
+	tag.classList.add('slide');
+
+	slideContainer.appendChild(tag);
+}
+
+setInterval(slideBackground, 10);
